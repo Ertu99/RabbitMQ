114a8a8f9242dd3704b664891a96b754070f1471
ertugrul <ertugrulaydin99@gmail.com>
2024-07-03 13:35:48 +0300

    rabbitmq

--- FanoutExchange/Producer/Program.cs
+++ FanoutExchange/Producer/Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
+using RabbitMQ.Client;
 
-Console.WriteLine("Hello, World!");
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    VirtualHost = "/",
+    Port = 5672,
+    UserName = "guest",
+    Password = "guest"
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare("ex.fanout","fanout",false,true);
+
+channel.QueueDeclare("queue.one",false,false,false);
+channel.QueueDeclare("queue.two",false,false,false);
+
+channel.QueueBind("queue.one","ex.fanout",string.Empty);
+channel.QueueBind("queue.two","ex.fanout",string.Empty);
+
+channel.BasicPublish("ex.fanout", string.Empty, false, null, Encoding.UTF8.GetBytes("This is a first fanout message."));
+channel.BasicPublish("ex.fanout", string.Empty, false, null, Encoding.UTF8.GetBytes("This is a second fanout message."));
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
+
+channel.QueueDelete("queue.one");
+channel.QueueDelete("queue.two");
+channel.ExchangeDelete("ex.fanout");
+
+channel.Close();
+connection.Close();
+
+
+
--- TopicExchange/Producer/Program.cs
+++ TopicExchange/Producer/Program.cs
@@ -1,3 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
+using RabbitMQ.Client;
 
-Console.WriteLine("Hello, World!");
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    VirtualHost = "/",
+    Port = 5672,
+    UserName = "guest",
+    Password = "guest"
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare("ex.topic", "topic", false, true);
+
+channel.QueueDeclare("log.error", true, false, false);
+channel.QueueDeclare("logs.all", true, false, false);
+channel.QueueDeclare("all.warnings", true, false, false);
+
+channel.QueueBind("log.error", "ex.topic", "log.error");
+channel.QueueBind("logs.all", "ex.topic", "log.*");
+channel.QueueBind("all.warnings", "ex.topic", "#.warning");
+
+channel.BasicPublish("ex.topic", "log", null, Encoding.UTF8.GetBytes("This is a log message"));
+channel.BasicPublish("ex.topic", "log.error", null, Encoding.UTF8.GetBytes("This is an error message"));
+channel.BasicPublish("ex.topic", "log.info", null, Encoding.UTF8.GetBytes("This is an info message"));
+channel.BasicPublish("ex.topic", "test.warning", null, Encoding.UTF8.GetBytes("This is a test warning message"));
+channel.BasicPublish("ex.topic", "warning", null, Encoding.UTF8.GetBytes("This is a warning message"));
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
+
+channel.QueueDelete("log.error");
+channel.QueueDelete("logs.all");
+channel.QueueDelete("all.warnings");
+channel.ExchangeDelete("ex.topic");
+
+channel.Close();
+connection.Close();
